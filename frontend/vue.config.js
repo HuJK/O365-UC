5e862234a4d1e3713fc5edd7f5a84364a000776b
HuJK <hu@whojk.com>
2020-09-06 04:23:04 +0800

    fix_pic

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,21 +2,31 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  chainWebpack: config => {
+    config.module.rule('images').use('url-loader')
+        .loader('url-loader')
+        .tap(options => {
+          options.limit = 1
+          options.fallback = {loader:"file-loader","options":{name: 'img/[name].[hash:8].[ext]'}};
+          return options
+        })
+  },
   "configureWebpack": {
-module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif|ico)$/i,
-          use: [
+    module: {
+          rules: [
             {
-              loader: 'file-loader',
-              options: {
-                name: 'files/[name].[ext]'
-              }
-            },
-          ],
+              test: /\.(png|jpe?g|gif|ico)$/i,
+              use: [
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 1,
+                    name: 'files/[name].[ext]'
+                  }
+                },
+              ],
+            }
+          ]
         }
-      ]
-    }
-  }
+      }
 }
